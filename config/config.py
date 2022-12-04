0ca2776acbd6635e2832bca259ea190923b89cd8
Nihal2873 <119762873+Nihal2873@users.noreply.github.com>
2022-12-04 09:06:36 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Nihal2873",
+    "https://github.com/Nihal2873/NihalMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
